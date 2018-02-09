8397357139cf8ac19f137f56c3d5fc2e932c0c75
Moshe <mmoshikoo@gmail.com>
2018-02-09 13:04:59 +0200

    Finished documentation for intergration

--- server/Integration/Emailer.ts
+++ server/Integration/Emailer.ts
@@ -4,11 +4,21 @@ import { TeacherInterface } from './../Interfaces/Teacher.interface';
 
 export class Emailer {
     //#region Public Methods
+    /**
+     * Sending Email to teacher given Email - Async.
+     * @param subject Subject of the Email.
+     * @param teacherModel Teacher model to export the details to build message.
+     */
     public async SendEmailToTeacherAsync(subject: string, teacherModel: TeacherInterface): Promise<void> {
         let body = '<div dir="ltr"></div>Hello ' + teacherModel.firstName + ' ' + teacherModel.lastName + ' and welcome to StudyHub.<br/> We hope you will find students from out application, improve your personal details and it will be fine.<br/>.<br/>Enjoy from StudyHub team and especially Moshe Binieli.<br/></div>';
         this.SendEmailAsync(teacherModel.email, subject, body);
     }
 
+    /**
+     * Sending Email to owner of app - Async.
+     * @param subject Subject of the Email.
+     * @param teacherModel Teacher model to export the details to build message.
+     */
     public async SendEmailToOwnerAsync(subject: string, teacherModel: TeacherInterface): Promise<void> {
         let email = "mmoshikoo@gmail.com";
         let body = 'Hey Moshe Binieli, new teacher has joined to your application, his name is ' + teacherModel.firstName + ' ' + teacherModel.lastName + ', you may see him at databases for more information, have a good day.';
@@ -17,6 +27,12 @@ export class Emailer {
     //#endregion
 
     //#region Private Methods
+    /**
+     * Function extues the email sending code - Async.
+     * @param {string} to Email as string.
+     * @param {string} subject Subject of the Email.
+     * @param {string} body Content of the Email.
+     */
     private SendEmailAsync(to: string, subject: string, body: string): Promise<void> {
         return new Promise((resolve, reject) => {
             let transporter = this.CreateTransport();
@@ -31,6 +47,10 @@ export class Emailer {
         });
     }
 
+    /**
+     * Builds the object with the data of the email sending from, classified details.
+     * @return Object with data.
+     */
     private CreateTransport(): any {
         return nodemailer.createTransport({
             service: 'Gmail',
@@ -41,6 +61,13 @@ export class Emailer {
         });
     }
 
+    /**
+     * Builds mail options.
+     * @param {string} to Email as string.
+     * @param {string} subject Subject of the Email.
+     * @param {string} body Content of the Email.
+     * @return Mail options.
+     */
     private MailOptions(to, subject, body): any {
         return {
             from: '"StudyHub 👻" <studyhubemail@gmail.com>',
